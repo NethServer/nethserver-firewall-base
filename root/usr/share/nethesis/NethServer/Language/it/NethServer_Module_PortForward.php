bb1207a1022d4953213fc45b75bbb23f097ce37f
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2013-04-18 10:38:05 +0200

    web ui translation: add en and it PortForward translation

---
+++ root/usr/share/nethesis/NethServer/Language/it/NethServer_Module_PortForward.php
@@ -0,0 +1,25 @@
+<?php 
+
+/* NethServer_Module_PortForward translation, language: it */
+
+$L['allow_label'] = 'Permetti solo da';
+$L['check-rules_Header'] = 'Controlla regole firewall';
+$L['CheckRules_label'] = 'Controlla firewall';
+$L['create_header_label'] = 'Crea port foward';
+$L['description_label'] = 'Descrizione';
+$L['disable_label'] = 'Disabilita';
+$L['dstHost_label'] = 'Host destinazione';
+$L['dst_label'] = 'Porta destinazione';
+$L['enable_label'] = 'Abilita';
+$L['Key_label'] = 'Id';
+$L['PortForward_Description'] = 'Gestici port forwarding';
+$L['PortForward_Tags'] = 'port port forward open';
+$L['PortForward_Title'] = 'Port Forward';
+$L['Proceed with port forward disable?'] = 'Disabilitare il port forward?';
+$L['Proceed with port forward enable?'] = 'Abilitare il port forward?';
+$L['proto_label'] = 'Protocollo';
+$L['shorewall_check_error'] = 'Shorewall ha ritornato un errore. Usare il pulsante "Controlla firewall" per maggiori informazioni.';
+$L['src_label'] = 'Porta di origine';
+$L['update_header_label'] = 'Modifica port forward';
+$L['tcp_label'] = 'TCP';
+$L['udp_label'] = 'UDP';
