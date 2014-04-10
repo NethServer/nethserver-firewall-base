af3a002d1f83edefa87b8c9e48d111f26d54b74b
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2014-04-10 09:35:59 +0200

    Web interface: add translations. Refs #2714

---
+++ root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_FirewallRules.php
@@ -0,0 +1,7 @@
+<?php 
+
+/* NethServer_Module_FirewallRules translation, language: en */
+
+$L['FirewallRules_Description'] = 'Manage firewall rules';
+$L['FirewallRules_Tags'] = 'firewall rule policy';
+$L['FirewallRules_Title'] = 'Firewall rules';
---
+++ root/usr/share/nethesis/NethServer/Language/it/NethServer_Module_FirewallRules.php
@@ -0,0 +1,7 @@
+<?php 
+
+/* NethServer_Module_FirewallRules translation, language: ir */
+
+$L['FirewallRules_Description'] = 'Gestione regole firewall';
+$L['FirewallRules_Tags'] = 'firewall rule policy regole';
+$L['FirewallRules_Title'] = 'Regole firewall';
