0713cf9665b9930dfc5006cd6c7e7777d5c50ae1
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2015-06-25 11:47:35 +0200

    Language: add missing labels, change title. Refs #3192

--- root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_Nat.php
+++ root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_Nat.php
@@ -1,7 +1,9 @@
 <?php
 
-$L['Nat_Title'] = "NAT 1:1";
-$L['NAT11_Configure_header'] = "NAT 1:1";
+$L['Nat_Title'] = "sNAT 1:1";
+$L['Nat_Description'] = "Source NAT 1:1";
+$L['Nat_Tags'] = "NAT source nat snat 1:1";
+$L['NAT11_Configure_header'] = "sNAT 1:1";
 $L['no_alias_found'] = "No alias found";
 $L['AliasIp_label'] = "Public IP alias";
 $L['FwObjectNat_label'] = "Local host";
