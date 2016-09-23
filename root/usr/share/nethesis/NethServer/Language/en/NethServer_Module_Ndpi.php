473f17c8abd2e783117ef4b9dcf114dfc02fb47b
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2016-09-23 10:29:52 +0200

    NethServer_Module_Ndpi: change title and add description

--- root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_Ndpi.php
+++ root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_Ndpi.php
@@ -2,5 +2,6 @@
 
 $L['Key_label'] = 'DPI protocol';
 $L['count_label'] = 'Count';
-$L['Ndpi_Title'] = 'DPI matches';
-$L['Ndpi_Tags'] = 'ndpi DPI dpi match';
+$L['Ndpi_Title'] = 'DPI';
+$L['Ndpi_Description'] = 'DPI status';
+$L['Ndpi_Tags'] = 'ndpi DPI dpi match count';
