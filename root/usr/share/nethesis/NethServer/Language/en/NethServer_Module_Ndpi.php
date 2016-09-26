6c6cdc4518c7770896987693fd8db00b2ad99f9c
Davide Principi <davide.principi@nethesis.it>
2016-09-26 10:53:29 +0200

    Move DPI page to nethserver-ndpi

--- root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_Ndpi.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$L['Key_label'] = 'DPI protocol';
-$L['count_label'] = 'Count';
-$L['Ndpi_Title'] = 'DPI';
-$L['Ndpi_Description'] = 'DPI status';
-$L['Ndpi_Tags'] = 'ndpi DPI dpi match count';
