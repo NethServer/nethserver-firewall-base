84a89046693a195913499c7519907827cc83f607
Stephane de Labrusse <stephdl@de-labrusse.fr>
2019-05-08 17:07:34 +0200

    Sudo permission for settings delegation

--- ui/src/views/Settings.vue
+++ ui/src/views/Settings.vue
@@ -208,7 +208,7 @@ export default {
         function(error) {
           console.error(error);
         },
-        false
+        true // sudo
       );
     },
     toggleSettingsMACValidation() {
@@ -261,7 +261,7 @@ export default {
             function(error, data) {
               console.error(error, data);
             },
-            false
+            true //sudo
           );
         },
         function(error, data) {
@@ -280,7 +280,7 @@ export default {
             console.error(e);
           }
         },
-        false
+        true //sudo 
       );
     }
   }
