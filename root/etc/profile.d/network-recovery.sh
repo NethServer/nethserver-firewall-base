0346baf70960da4a82416162318531a65058520a
Davide Principi <davide.principi@nethesis.it>
2019-11-18 18:18:07 +0100

    Fix hint text

--- root/etc/profile.d/network-recovery.sh
+++ root/etc/profile.d/network-recovery.sh
@@ -3,7 +3,7 @@ tty=$(/usr/bin/tty)
 if [[ "$USER" = "root" ]] && [[ "${tty}" = /dev/tty* ]]; then
     echo ''
     echo '(!) Hint'
-    echo '    Run the "network-recovery" command '
+    echo '    In case of network troubles, run the "network-recovery" command '
     echo '    to quickly assign a temporary IP address'
     echo ''
 fi
